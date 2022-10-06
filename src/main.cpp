180551411598063ec6a3a5a89c79469e9134bae5
nuxarch <misbaqul4sfi4k>
2022-10-06 12:21:10 +0700

    modify PP number again

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,4 @@
-/**
- * Comprehensive BLDC motor control example using encoder and the DRV8302 board
- *
- * Using serial terminal user can send motor commands and configure the motor and FOC in real-time:
- * - configure PID controller constants
- * - change motion control loops
- * - monitor motor variabels
- * - set target values
- * - check all the configuration values
- *
- * check the https://docs.simplefoc.com for full list of motor commands
- *
- */
 #include <SimpleFOC.h>
-
-// DRV8302 pins connections
-// don't forget to connect the common ground pin
 #define INH_A 25
 #define INH_B 26
 #define INH_C 27
@@ -23,41 +7,25 @@
 #define M_PWM 19
 #define M_OC 18
 #define OC_ADJ 21
-
-// Motor instance
-BLDCMotor motor = BLDCMotor(12,5);
+BLDCMotor motor = BLDCMotor(13);
 BLDCDriver3PWM driver = BLDCDriver3PWM(INH_A, INH_B, INH_C, EN_GATE);
-
-// SENSOR
-HallSensor sensor = HallSensor(32, 35, 34, 23);
+HallSensor sensor = HallSensor(32, 35, 34, 13);
 void doA() { sensor.handleA(); }
 void doB() { sensor.handleB(); }
 void doC() { sensor.handleC(); }
-
-// commander interface
 Commander command = Commander(Serial);
 void onMotor(char *cmd) { command.motor(&motor, cmd); }
 void setup() {
-  // monitoring port
   Serial.begin(115200);
-
-  // check if you need internal pullups
-  sensor.pullup = Pullup::USE_EXTERN;
-  
-  // initialise encoder hardware
+  sensor.pullup = Pullup::USE_INTERN;
   sensor.init();
-  // hardware interrupt enable
   sensor.enableInterrupts(doA, doB, doC);
-
   Serial.println("Sensor ready");
   _delay(1000);
 }
 
 void loop() {
-  // iterative function updating the sensor internal variables
-  // it is usually called in motor.loopFOC()
   sensor.update();
-  // display the angle and the angular velocity to the terminal
   Serial.print(sensor.getAngle());
   Serial.print("\t");
   Serial.println(sensor.getVelocity());
